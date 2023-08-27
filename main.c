10647e3a229662539399e34029c494639cebf651
Conrad Korzon <conrad.korzon@gmail.com>
2023-08-27 19:45:11 -0400

    fix: struct for TuringMachine, method for constructing dynamic tape

--- main.c
+++ main.c
@@ -12,6 +12,15 @@ typedef struct Card {
     cardrow rows[2];
 } card;
 
+typedef struct TuringMachine {
+    int currentCard;
+    int tapeIndex;
+    int numCards;
+    int tapeLength;
+    bool *tape;
+    card *cards;
+} turingmachine;
+
 void printCardRow(bool n, cardrow crdrow) {
     printf("|%d : %d %d %d|\n", n, crdrow.writeVal, crdrow.shiftRight, crdrow.nextCard);
 }
@@ -22,12 +31,45 @@ void printCard(int crdNum, card crd) {
     printCardRow(0, crd.rows[0]);
     printCardRow(1, crd.rows[1]);
     printf("===========\n");
-
 }
+
+void printTape(turingmachine *tm) {
+    for (int i=0; i<tm->tapeLength; i++) {
+        printf("%d ", tm->tape[i]);
+    }
+}
+
+void showExampleCards() {
+    // Example Cards:
+    cardrow zeroRow = { 1, 1, 2 };
+    cardrow oneRow = { 0, 0, 0 };
+    card firstcard = { { zeroRow, oneRow } };
+
+    cardrow zeroRowTwo = { 1, 0, 1 };
+    cardrow oneRowTwo = { 0, 1, 2 };
+    card secondcard = { { zeroRowTwo, oneRowTwo } };
+
+    card cards[2] = { firstcard, secondcard };
+
+    printf("<> CARDS: <>\n");
+    printCard(0, cards[0]);
+    printCard(1, cards[1]);
+}
+
+void constructTape(bool *tape, int tapeLength) {
+    for (int i=0; i<tapeLength; i++) {
+        tape[i] = 0;
+    }
+}
+
+// void turingStep(int *index, )
 
 int main(int argc, char* argv[]) {
     // Simulate a Binary turing machine with N states
-    // Components = tape, cards/states, 
+    // Components = tape, cards/states
+    // Card Syntax:
+    // 0 : 110 (if read 0: write 1, move right, halt)
+    // 1 : 101 (if read 1: write 1, move left, go to card 1)
 
     int tapeLength;
     switch(argc) {
@@ -43,29 +85,29 @@ int main(int argc, char* argv[]) {
             return 1;
     }
 
-    bool tape[tapeLength];
-
-    // Card Syntax:
-    // 0 : 110 (if read 0: write 1, move right, halt)
-    // 1 : 101 (if read 1: write 1, move left, go to card 1)
-
-    // TODO: Take a list of numbers from user and convert to set of cards.
-    // TODO: OR read cards from a file
-
-    // Example Cards:
+    //Placeholder single card until dynamic card loading is implemented
     cardrow zeroRow = { 1, 1, 2 };
     cardrow oneRow = { 0, 0, 0 };
     card firstcard = { { zeroRow, oneRow } };
 
-    cardrow zeroRowTwo = { 1, 0, 1 };
-    cardrow oneRowTwo = { 0, 1, 2 };
-    card secondcard = { { zeroRowTwo, oneRowTwo } };
+    // Declare tape and cards
+    bool tape[tapeLength];
+    constructTape(tape, tapeLength);
+    card cards[1];
+    cards[0] = firstcard;
 
-    card cards[2] = { firstcard, secondcard };
+    // Construct the turing machine
+    turingmachine tm;
+    tm.tapeLength = tapeLength;
+    tm.tape = tape;
+    tm.numCards = 1;
+    tm.cards = cards;
 
-    printf("<> CARDS: <>\n");
-    printCard(0, cards[0]);
-    printCard(1, cards[1]);
+    // For now, just print the tape
+    printTape(&tm);
+
+    // TODO: Take a list of numbers from user and convert to set of cards.
+    // TODO: OR read cards from a file
     
     return 0;
 }
