63a8643a4fb1af91c14a0c46ed1f075e76ea858e
Conrad Korzon <conrad.korzon@gmail.com>
2023-08-28 21:39:43 -0400

    feat: Runnable turing machine. Added methods for initialize, run, iterate, and step.

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+const int MAX_ITERATIONS = 1000;
+
 typedef struct CardRow {
     bool writeVal;
     bool shiftRight;
@@ -20,6 +22,12 @@ typedef struct TuringMachine {
     bool *tape;
     card *cards;
 } turingmachine;
+
+void constructTape(bool *tape, int tapeLength) {
+    for (int i=0; i<tapeLength; i++) {
+        tape[i] = 0;
+    }
+}
 
 void printCardRow(bool n, cardrow crdrow) {
     printf("|%d : %d %d %d|\n", n, crdrow.writeVal, crdrow.shiftRight, crdrow.nextCard);
@@ -56,13 +64,54 @@ void showExampleCards() {
     printCard(1, cards[1]);
 }
 
-void constructTape(bool *tape, int tapeLength) {
-    for (int i=0; i<tapeLength; i++) {
-        tape[i] = 0;
-    }
+card* getCurrentCard(turingmachine *tm) {
+    return &(tm->cards[tm->currentCard]);
 }
 
-// void turingStep(int *index, )
+void initTuringMachine(turingmachine *tm, card *cards, int tapeLength) {
+    bool tape[tapeLength];
+    constructTape(tape, tapeLength);
+    tm->tapeLength = tapeLength;
+    tm->tape = tape;
+    tm->numCards = sizeof(*cards) / sizeof(card);
+    tm->cards = cards;
+    tm->currentCard = 0;
+    tm->tapeIndex = tapeLength / 2;
+}
+
+void doTuringStep(turingmachine *tm, cardrow *operation) {
+    // Write the next value per card row instructions
+    tm->tape[tm->tapeIndex] = operation->writeVal;
+    printf("> Wrote %d at position %d. Moving %d (0=left, 1=right), next card = %d\n", operation->writeVal, tm->tapeIndex, operation->shiftRight, operation->nextCard);
+    // Move the tape index left or right (branchlessly)
+    tm->tapeIndex = tm->tapeIndex + (2 * (operation->shiftRight) - 1);
+    // Determine if we halt, or move to next card
+    tm->currentCard = operation->nextCard;
+}
+
+void turingIterate(turingmachine *tm) {
+    card *currentCard = getCurrentCard(tm);
+    bool readVal = tm->tape[tm->tapeIndex];
+    // *Note: We're heavily taking advantage of C's bool type equivalence to 0/1
+    // true => 1, false => 0
+    doTuringStep(tm, &(currentCard->rows[readVal]));
+}
+
+void runTuringMachine(turingmachine *tm) {
+    int iterations = 1;
+    while (tm->currentCard > -1) {
+        turingIterate(tm);
+        if (tm->currentCard < 0) {
+            printf("Success! Turing machine halted at iteration %d.\n", iterations);
+            return;
+        } 
+        iterations++;
+        if (iterations > MAX_ITERATIONS) {
+            printf("WARNING: Too many turing iterations, Aborting!\n");
+            break;
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
     // Simulate a Binary turing machine with N states
@@ -86,25 +135,20 @@ int main(int argc, char* argv[]) {
     }
 
     //Placeholder single card until dynamic card loading is implemented
-    cardrow zeroRow = { 1, 1, 2 };
+    cardrow zeroRow = { 1, 1, -1 };
     cardrow oneRow = { 0, 0, 0 };
     card firstcard = { { zeroRow, oneRow } };
 
-    // Declare tape and cards
-    bool tape[tapeLength];
-    constructTape(tape, tapeLength);
+    // Declare cards
     card cards[1];
     cards[0] = firstcard;
 
     // Construct the turing machine
     turingmachine tm;
-    tm.tapeLength = tapeLength;
-    tm.tape = tape;
-    tm.numCards = 1;
-    tm.cards = cards;
+    initTuringMachine(&tm, cards, tapeLength);
 
     // For now, just print the tape
-    printTape(&tm);
+    runTuringMachine(&tm);
 
     // TODO: Take a list of numbers from user and convert to set of cards.
     // TODO: OR read cards from a file
