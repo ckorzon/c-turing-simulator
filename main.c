26335eb060f4551c665f5090a13bd1916bbaece2
Conrad Korzon <conrad.korzon@gmail.com>
2024-09-05 23:02:56 -0400

    fix: Proper memory allocation/deallocation for tape, handle tape index out of bounds, use two cards.

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ typedef struct Card {
 } card;
 
 typedef struct TuringMachine {
-    int currentCard;
+    int cardIndex;
     int tapeIndex;
     int numCards;
     int tapeLength;
@@ -23,92 +23,99 @@ typedef struct TuringMachine {
     card *cards;
 } turingmachine;
 
-void constructTape(bool *tape, int tapeLength) {
+bool* constructTape(int tapeLength) {
+    bool * tape = (bool *) malloc(tapeLength * sizeof(bool));
+    if (tape == NULL) {
+        printf("Memory allocation failed for Turing Machine tape!\n");
+        exit(1);
+    }
     for (int i=0; i<tapeLength; i++) {
         tape[i] = 0;
     }
+    return tape;
 }
 
 void printCardRow(bool n, cardrow crdrow) {
     printf("|%d : %d %d %d|\n", n, crdrow.writeVal, crdrow.shiftRight, crdrow.nextCard);
 }
 
-void printCard(int crdNum, card crd) {
+void printCard(int crdNum, card * crd) {
     printf("===========\n");
     printf("|# Card%d #|\n", crdNum);
-    printCardRow(0, crd.rows[0]);
-    printCardRow(1, crd.rows[1]);
+    printCardRow(0, crd->rows[0]);
+    printCardRow(1, crd->rows[1]);
     printf("===========\n");
 }
 
 void printTape(turingmachine *tm) {
-    for (int i=0; i<tm->tapeLength; i++) {
+    for (int i=0; i < tm->tapeLength; i++) {
         printf("%d ", tm->tape[i]);
     }
 }
 
-void showExampleCards() {
-    // Example Cards:
-    cardrow zeroRow = { 1, 1, 2 };
-    cardrow oneRow = { 0, 0, 0 };
-    card firstcard = { { zeroRow, oneRow } };
-
-    cardrow zeroRowTwo = { 1, 0, 1 };
-    cardrow oneRowTwo = { 0, 1, 2 };
-    card secondcard = { { zeroRowTwo, oneRowTwo } };
-
-    card cards[2] = { firstcard, secondcard };
-
-    printf("<> CARDS: <>\n");
-    printCard(0, cards[0]);
-    printCard(1, cards[1]);
-}
-
 card* getCurrentCard(turingmachine *tm) {
-    return &(tm->cards[tm->currentCard]);
+    return &(tm->cards[tm->cardIndex]);
 }
 
-void initTuringMachine(turingmachine *tm, card *cards, int tapeLength) {
-    bool tape[tapeLength];
-    constructTape(tape, tapeLength);
+void initializeTuringMachine(turingmachine *tm, card *cards, int tapeLength) {
+    // The tape is an array of 0's and 1's, represented here as booleans.
     tm->tapeLength = tapeLength;
-    tm->tape = tape;
+    tm->tape = constructTape(tm->tapeLength);
     tm->numCards = sizeof(*cards) / sizeof(card);
     tm->cards = cards;
-    tm->currentCard = 0;
+    tm->cardIndex = 0;
     tm->tapeIndex = tapeLength / 2;
 }
 
-void doTuringStep(turingmachine *tm, cardrow *operation) {
+void dismantleTuringMachine(turingmachine *tm) {
+    free(tm->tape);
+}
+
+void followCardInstructions(turingmachine *tm, cardrow *operation) {
     // Write the next value per card row instructions
     tm->tape[tm->tapeIndex] = operation->writeVal;
-    printf("> Wrote %d at position %d. Moving %d (0=left, 1=right), next card = %d\n", operation->writeVal, tm->tapeIndex, operation->shiftRight, operation->nextCard);
+    int nextPosition = tm->tapeIndex + (2 * (operation->shiftRight) - 1);
+    printf("> Wrote %d at position %d. Next position = %d, next card = %d.\n", operation->writeVal, tm->tapeIndex, nextPosition, operation->nextCard);
     // Move the tape index left or right (branchlessly)
-    tm->tapeIndex = tm->tapeIndex + (2 * (operation->shiftRight) - 1);
-    // Determine if we halt, or move to next card
-    tm->currentCard = operation->nextCard;
+    tm->tapeIndex = nextPosition;
+    // Find next card index. Offset by -1 since 0 represents halt and 1 represents first card (index 0).
+    tm->cardIndex = operation->nextCard - 1;
 }
 
 void turingIterate(turingmachine *tm) {
-    card *currentCard = getCurrentCard(tm);
     bool readVal = tm->tape[tm->tapeIndex];
+    printf("> Read value: %d.\n", readVal);
+    if (!(readVal == 0 || readVal == 1)) {
+        printf("INVALID VALUE READ FROM TAPE: %d.\n", readVal);
+        EXIT_FAILURE;
+    }
+    card *currentCard = getCurrentCard(tm);
     // *Note: We're heavily taking advantage of C's bool type equivalence to 0/1
     // true => 1, false => 0
-    doTuringStep(tm, &(currentCard->rows[readVal]));
+    followCardInstructions(tm, &(currentCard->rows[readVal]));
 }
 
 void runTuringMachine(turingmachine *tm) {
+    printf("### RUNNING TURING MACHINE ###\n");
     int iterations = 1;
-    while (tm->currentCard > -1) {
+    bool run = true;
+    while (run) {
         turingIterate(tm);
-        if (tm->currentCard < 0) {
-            printf("Success! Turing machine halted at iteration %d.\n", iterations);
-            return;
-        } 
+        // Check if our card told us to halt
+        if (tm->cardIndex < 0) {
+            printf("SUCCESS: Turing machine halted at iteration %d.\n", iterations);
+            run = false;
+        }
+        // Check if we've run out of tape
+        if (tm->tapeIndex >= tm->tapeLength || tm->tapeIndex < 0) {
+            printf("ABORT: Next position %d is not on the tape, cannot continue.\n", tm->tapeIndex);
+            run = false;
+        }
+        // Check if we've hit maximum iterations
         iterations++;
         if (iterations > MAX_ITERATIONS) {
-            printf("WARNING: Too many turing iterations, Aborting!\n");
-            break;
+            printf("ABORT: Too many turing iterations.\n");
+            run = false;
         }
     }
 }
@@ -134,21 +141,26 @@ int main(int argc, char* argv[]) {
             return 1;
     }
 
-    //Placeholder single card until dynamic card loading is implemented
-    cardrow zeroRow = { 1, 1, -1 };
-    cardrow oneRow = { 0, 0, 0 };
-    card firstcard = { { zeroRow, oneRow } };
-
     // Declare cards
-    card cards[1];
-    cards[0] = firstcard;
+    cardrow zeroRow = { 1, 0, 2 };
+    cardrow oneRow = { 0, 0, 2 };
+    card firstCard = { { zeroRow, oneRow } };
+
+    cardrow zeroRowB = { 1, 1, 1 };
+    cardrow oneRowB = { 0, 1, 0 };
+    card secondCard = { { zeroRowB, oneRowB } };
+
+    card cards[] = { firstCard, secondCard };
 
     // Construct the turing machine
     turingmachine tm;
-    initTuringMachine(&tm, cards, tapeLength);
+    initializeTuringMachine(&tm, cards, tapeLength);
 
-    // For now, just print the tape
+    // Run the turing machine
     runTuringMachine(&tm);
+
+    // Deconstruct to free memory
+    dismantleTuringMachine(&tm);
 
     // TODO: Take a list of numbers from user and convert to set of cards.
     // TODO: OR read cards from a file
